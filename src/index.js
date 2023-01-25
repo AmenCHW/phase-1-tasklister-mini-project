27c878c831ee83272a148aeed77e06bbb3d89719
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-25 19:58:55 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    buildToDO(e.target.description.value)
+      form.reset()
+  })
+  
 });
+
+function buildToDO(toDO) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "X"
+  p.textContent = `${toDO} `
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
